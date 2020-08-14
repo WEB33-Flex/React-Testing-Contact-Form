068b2fa011fd9dbb55a45a815810c895c10f4d6b
Jose Carlos Robles <jcrobles1989@gmail.com>
2020-08-13 19:50:46 -0700

    all tests passing

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -3,6 +3,21 @@ import { render, screen, fireEvent, act } from "@testing-library/react";
 import ContactForm from "../components/ContactForm";
 
 test("ContactForm accepts user data and renders it on the page", async () => {
+  render(<ContactForm />);
 
-    render(<ContactForm />);
-})
+  const firstNameInput = screen.getByLabelText(/first name*/i);
+  const lastNameInput = screen.getByLabelText(/last name*/i);
+  const emailInput = screen.getByLabelText(/email*/i);
+  const messageInput = screen.getByLabelText(/message/i);
+
+  fireEvent.change(firstNameInput, { target: { value: "Jose" } });
+  fireEvent.change(lastNameInput, { target: { value: "Robles" } });
+  fireEvent.change(emailInput, { target: { value: "jose@email.com" } });
+  fireEvent.change(messageInput, { target: { value: "Hello World!" } });
+
+  const submitBtn = screen.getByRole("button", { type: /submit/i });
+
+  await act(async () => {
+    fireEvent.click(submitBtn);
+  });
+});
